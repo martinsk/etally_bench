6a14edb2048e75ffc8c88c4c151fb75eecc9abbb
martinsk <msk@tigertext.com>
2014-01-18 21:49:52 -0800

    moved the instance name into the app.config

--- apps/etally_bench/src/etally_bench_suite_srv.erl
+++ apps/etally_bench/src/etally_bench_suite_srv.erl
@@ -13,31 +13,33 @@ start_link(Args) -> gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
 init([]) ->
     %% test before start command,
+    {ok,Instance} = application:get_env(etally_bench, etally_instance),
+    io:fwrite("working with node : ~p~n", [Instance]),
     _ = [begin
              Fun = fun(0, F) -> ok;
                       (X, F) ->
-                           etally_count:send_event([<<"test_event">>], [{<<"test_event">>, <<"test_leaderboard_1">>}], 'tally@msk-dev.local'),
+                           etally_count:send_event([<<"test_event">>], [{<<"test_event">>, <<"test_leaderboard_1">>}], Instance),
                            F(X-1, F)
                    end,
              Scaled_X = 1000*X,
              {Time, Res} = timer:tc(fun () ->
                                             ok = Fun(Scaled_X, Fun),
-                                            _ = etally_count:get_counter(<<"test_event">>, 'tally@msk-dev.local')
+                                            _ = etally_count:get_counter(<<"test_event">>, Instance)
                                     end),
              io:fwrite("~p inserts in ~p mics - ~p ops/sec ~n", [Scaled_X, Time, Scaled_X*1000000.0/Time] )
          end || X <- lists:seq(1,10)],
     {any, 'tally@msk-dev.local'} ! {cast, self(), {system_start} },
-    _ = etally_count:get_counter(<<"test_event">>, 'tally@msk-dev.local'),
+    _ = etally_count:get_counter(<<"test_event">>, Instance),
     _ = [begin
              Fun = fun(0, F) -> ok;
                       (X, F) ->
-                           etally_count:send_event([<<"test_event_2">>], [{<<"test_event_2">>, <<"test_leaderboard_2">>}], 'tally@msk-dev.local'),
+                           etally_count:send_event([<<"test_event_2">>], [{<<"test_event_2">>, <<"test_leaderboard_2">>}], Instance),
                            F(X-1, F)
                    end,
              Scaled_X = 1000*X,
              {Time, Res} = timer:tc(fun () ->
                                             ok = Fun(Scaled_X, Fun),
-                                            _ = etally_count:get_counter(<<"test_event_2">>, 'tally@msk-dev.local')
+                                            _ = etally_count:get_counter(<<"test_event_2">>, Instance)
                                     end),
              io:fwrite("~p inserts w. leaderboard in ~p mics - ~p ops/sec ~n", [Scaled_X, Time, Scaled_X*1000000.0/Time] )
          end || X <- lists:seq(1,10)],
